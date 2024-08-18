31c587a2cb0cc29825e01ff2197edbaa7aa0cfc1
umiushi <umiushi.me+dev@gmail.com>
2024-08-18 23:09:25 +0900

    feat: add handling error sample

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 //TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
@@ -19,7 +20,27 @@ func main() {
 		// right-click your code in the editor and select the <b>Debug</b> option.
 		fmt.Println("i =", 100/i)
 	}
+
+	errorHandle("sample.txt")
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
 // Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+
+func errorHandle(fname string) error {
+	// エラーは状態であり表現
+	// Goにおいてはエラーをerrorインターフェースの実装された値として返却して、それを実装者が愚直にハンドリングすることをエラー処理と見なしているだけで言語としてエラー処理の仕組みがある訳ではない
+	f, err := os.Create(fname)
+	if err != nil {
+		return fmt.Errorf("can not create file %s: %w", fname, err)
+	}
+
+	defer f.Close()
+
+	_, err = f.WriteAt([]byte("sample"), 0)
+	if err != nil {
+		return fmt.Errorf("can not write file %s: %w", fname, err)
+	}
+
+	return nil
+}
